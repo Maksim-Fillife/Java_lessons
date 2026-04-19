17ba5ed48b33c99475bb9955a2a23688d900432a
Filatov <fillife.one@ya.ru>
2026-04-19 14:02:28 +0300

    add task

--- src/test/java/tests_by_topic/ConditionalConstruction.java
+++ src/test/java/tests_by_topic/ConditionalConstruction.java
@@ -1,4 +1,25 @@
 package tests_by_topic;
+import java.util.Scanner;
 
-public class ConditionalConstruction {
+class ConditionalConstruction {
+    public static void main(String[] ars) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+        }
+    }
 }
+
+//На вход подаётся  целое число. Выведите "YES", если введено трёхзначное положительное число, а в противном случае - "NO".
+//class ConditionalConstruction {
+//    public static void main(String[] ars) {
+//        try (Scanner sc = new Scanner(System.in)) {
+//            int num = sc.nextInt();
+//
+//            if (num >= 100 && num < 1000) {
+//                System.out.println("YES");
+//            } else {
+//                System.out.println("NO");
+//            }
+//        }
+//    }
+//}
