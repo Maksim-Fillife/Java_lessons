4440b4a48149c4c4ede1ca54904b005c41e86dc8
Filatov <fillife.one@ya.ru>
2026-04-19 15:04:57 +0300

    Решение тестовых задач

--- src/test/java/tests_by_topic/ConditionalConstruction.java
+++ src/test/java/tests_by_topic/ConditionalConstruction.java
@@ -2,16 +2,31 @@ package tests_by_topic;
 import java.util.Scanner;
 
 class ConditionalConstruction {
-    public static void main(String[] ars) {
+    public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+            String str = sc.nextLine().toLowerCase();
+            String word = sc.next().toLowerCase();
 
+            System.out.println(str.contains(word));
         }
     }
 }
 
+//На вход подается строка, а затем слово. Выведите true, если слово содержится в строке, и false - если нет, без учёта регистров.
+//class ConditionalConstruction {
+//    public static void main(String[] args) {
+//        try (Scanner sc = new Scanner(System.in)) {
+//            String str = sc.nextLine().toLowerCase();
+//            String word = sc.next().toLowerCase();
+//
+//            System.out.println(str.contains(word));
+//        }
+//    }
+//}
+
 //На вход подаётся  целое число. Выведите "YES", если введено трёхзначное положительное число, а в противном случае - "NO".
 //class ConditionalConstruction {
-//    public static void main(String[] ars) {
+//    public static void main(String[] args) {
 //        try (Scanner sc = new Scanner(System.in)) {
 //            int num = sc.nextInt();
 //
